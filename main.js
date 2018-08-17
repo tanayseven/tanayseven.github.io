b27350227e1acefad229c7ad92bd3bd49a79fda8
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-08-17 11:30:03 +0530

    M  blog/2016-03-27-hello-world.html M  blog/2016-07-14-finally-an-android-app.html M  blog/2016-07-18-at-nelkinda-coderetreat.html M  blog/2018-01-03-sicp-challenge.html M  blog/index.html M  index.html M  main.css M  main.js M  resume/index.html

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
-$(document).ready(function(){
-  $('.collapsible').collapsible({
-    accordion: true,
-  });
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-})
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  const sideNavOptions = {};
+  const sideNavInstances = M.Sidenav.init(elems, sideNavOptions);
+  var elems = document.querySelectorAll('.collapsible');
+  const collapsibleOptions = {accordion: true};
+  const collapsibleInstances = M.Collapsible.init(elems, collapsibleOptions)
+
+  var targetOffset, currentPosition,
+      body = document.body,
+      button = document.getElementById('scrollButton'),
+      animateTime = 900;
+
+  function getPageScroll() {
+    var yScroll;
+
+    if (window.pageYOffset) {
+      yScroll = window.pageYOffset;
+    } else if (document.documentElement && document.documentElement.scrollTop) {
+      yScroll = document.documentElement.scrollTop;
+    } else if (document.body) {
+      yScroll = document.body.scrollTop;
+    }
+    return yScroll;
+  }
+
+  button.addEventListener('click', function (event) {
+
+    targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
+    currentPosition = getPageScroll();
+
+    body.classList.add('in-transition');
+    body.style.WebkitTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+    body.style.MozTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+    body.style.transform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+
+    window.setTimeout(function () {
+      body.classList.remove('in-transition');
+      body.style.cssText = "";
+      window.scrollTo(0, targetOffset);
+    }, animateTime);
+
+    event.preventDefault();
+
+  }, false);
+});
