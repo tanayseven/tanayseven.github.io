af12f53da27030442d4f72129fb1dee69d5461e3
Tanay PrabhuDesai <tanayseven@gmail.com>
2018-08-19 19:51:41 +0530

    A  contact_form.txt M  index.html M  main.css M  main.js

--- main.js
+++ main.js
@@ -6,41 +6,45 @@ document.addEventListener('DOMContentLoaded', function() {
   const collapsibleOptions = {accordion: true};
   const collapsibleInstances = M.Collapsible.init(elems, collapsibleOptions)
 
-  var targetOffset, currentPosition,
-      body = document.body,
-      button = document.getElementById('scrollButton'),
-      animateTime = 900;
+  // var targetOffset, currentPosition,
+  //     body = document.body,
+  //     button = document.getElementById('scrollButton'),
+  //     animateTime = 900;
 
-  function getPageScroll() {
-    var yScroll;
+  // function getPageScroll() {
+  //   var yScroll;
 
-    if (window.pageYOffset) {
-      yScroll = window.pageYOffset;
-    } else if (document.documentElement && document.documentElement.scrollTop) {
-      yScroll = document.documentElement.scrollTop;
-    } else if (document.body) {
-      yScroll = document.body.scrollTop;
-    }
-    return yScroll;
-  }
+  //   if (window.pageYOffset) {
+  //     yScroll = window.pageYOffset;
+  //   } else if (document.documentElement && document.documentElement.scrollTop) {
+  //     yScroll = document.documentElement.scrollTop;
+  //   } else if (document.body) {
+  //     yScroll = document.body.scrollTop;
+  //   }
+  //   return yScroll;
+  // }
 
-  button.addEventListener('click', function (event) {
+  // button.addEventListener('click', function (event) {
 
-    targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
-    currentPosition = getPageScroll();
+  //   targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
+  //   currentPosition = getPageScroll();
 
-    body.classList.add('in-transition');
-    body.style.WebkitTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
-    body.style.MozTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
-    body.style.transform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+  //   body.classList.add('in-transition');
+  //   body.style.WebkitTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+  //   body.style.MozTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+  //   body.style.transform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
 
-    window.setTimeout(function () {
-      body.classList.remove('in-transition');
-      body.style.cssText = "";
-      window.scrollTo(0, targetOffset);
-    }, animateTime);
+  //   window.setTimeout(function () {
+  //     body.classList.remove('in-transition');
+  //     body.style.cssText = "";
+  //     window.scrollTo(0, targetOffset);
+  //   }, animateTime);
 
-    event.preventDefault();
+  //   event.preventDefault();
 
-  }, false);
+  // }, false);
+  const contact_form_request=new XMLHttpRequest();
+  contact_form_request.open('get','/contact_form.txt');
+  contact_form_request.send();
+  contact_form_request.onload=function(){document.getElementById('contact').innerHTML=contact_form_request.responseText}
 });
