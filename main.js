2bff9df0d47447f6146834ea1412aa0d289d8385
Tanay PrabhuDesai <tanayseven@gmail.com>
2019-02-01 10:32:26 +0530

    M  blog/2016-03-27-hello-world.html M  blog/2016-07-14-finally-an-android-app.html M  blog/2016-07-18-at-nelkinda-coderetreat.html M  blog/2018-01-03-sicp-challenge.html A  blog/2018-09-18-scheme-crash-course.html M  blog/index.html A  dracula.css A  highlight.js M  index.html M  main.css M  main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const setContactEmailAddress = function () {
+  const emailElement = document.querySelector('#email-link')
+  const obfuscatedEmailAddress = 'abcdtabcdanabcdayabcdseabcdveabcdn@gmail.com'
+  emailElement.innerHTML = obfuscatedEmailAddress.replace(/abcd/g, '')
+  emailElement.setAttribute('href', 'mailto:'+obfuscatedEmailAddress.replace(/abcd/g, ''))
+}
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   const sideNavOptions = {};
@@ -5,8 +11,5 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   const collapsibleOptions = {accordion: true};
   const collapsibleInstances = M.Collapsible.init(elems, collapsibleOptions)
-  const contact_form_request=new XMLHttpRequest();
-  contact_form_request.open('get','/contact_form.txt');
-  contact_form_request.send();
-  contact_form_request.onload=function(){document.getElementById('contact').innerHTML=contact_form_request.responseText}
+  setContactEmailAddress();
 });
